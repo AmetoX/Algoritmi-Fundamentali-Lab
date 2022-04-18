ae51cef2f759145e5d319847ac76d20223c6c866
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-04-18 11:38:53 +0300

    lab

---
+++ ConturMatrice_lab/Program.cs
@@ -0,0 +1,50 @@
+﻿int n = 10;
+int m = 13;
+int[,] matrice= new int[n,m];
+int d = 1;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (i==1 &&i - 1 >= 0 && j - 1 >= 0&&j<m-2)
+        {
+            matrice[i, j] = d;
+            d++;
+        }
+        if (j == m - 2&&i!=0 && i<n-1)
+        {
+            matrice[i, j] = d;
+            d++;
+        }
+    }    
+}
+for (int i = n-1; i > 0; i--)
+{
+    for (int j = m-1; j > 0; j--)
+    {
+        if(i==n-2 && j <= m - 3)
+        {
+            matrice[i, j] = d;
+            d++;
+        }
+        if(i<n-2 && j<n-1 && i>1 && j<2)
+        {
+            matrice[i, j] = d;
+            d++;
+        }
+    }
+}
+
+viewMatrix(matrice);
+
+static void viewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ ZonaMatrice_lab/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 11;//linii
+int m = 11;//coloane
+int[,] matrice = new int[n,m];
+viewMatrix(matrice);
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+
+        if (i < j && i + j < n/2+1)
+            matrice[i, j] = 1;
+        if (i < j && i + j > n / 2 + 1)
+            matrice[i, j] = 1;
+        if (i < j/2 && i + j > n/2 - 1)
+            
+        if (i > j/2 && i + j > n/2 - 1)
+            
+        if (i > j && i + j < n/2 - 1)
+            matrice[i, j] = 4;
+    }
+}
+viewMatrix(matrice);
+
+static void viewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+        
